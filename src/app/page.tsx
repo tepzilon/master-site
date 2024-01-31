236deaeb9f1c1bacfeb95e1fff1c606055992fd9
tepzilon <tepzilon@gmail.com>
2024-02-01 01:05:40 +0700

    cdn invalid cache test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className={styles['under-construction']}>👷‍♂️ This page is under construction. 🚧</p>
+      <p className={styles['under-construction']}>👷‍♂️ This page is under construction. CDN test 1 🚧</p>
     </main>
   );
 }
